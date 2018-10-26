11524982c9104fd45347a5d647385eabdf555c64
Norbaeocystin <baranrastislav@gmail.com>
2018-10-26 17:23:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="controlberry_pkg",
+    install_requires=["pymongo","RPi"],
     version="0.0.1",
     author="Rastislav_Baran",
     author_email="baranrastislav@gmail.com",
     description="Package which needs to be installed on Raspberry Pi 3 to control Raspberry Pi 3 by MongoDB ",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    package_data={'Controlberry': ['*.json']},
+    entry_points={'console_scripts':['controlberry = Controlberry.control']},
     url="",
     packages=setuptools.find_packages(),
     classifiers=[
