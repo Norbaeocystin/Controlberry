e4c8892117551c5a71004551a93324e85e809b37
Norbaeocystin <baranrastislav@gmail.com>
2018-10-26 22:09:40 +0200

    Update adafruit.py

--- Controlberry/adafruit.py
+++ Controlberry/adafruit.py
@@ -1,1 +1,14 @@
+"""
+to control adafruit  sensors for temperature and humidity
+"""
+import Adafruit_DHT
 
+
+# Parse command line parameters.
+sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302 }
+
+def get_data(sensor, pin):
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	return {"Humidity":humidity, "Temperature":temperature}
