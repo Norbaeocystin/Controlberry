09a4c8428c7824a6a349e62ccb591802bccf7b5b
Norbaeocystin <baranrastislav@gmail.com>
2018-10-27 02:07:58 +0200

    Update temperature.py

--- Controlberry/temperature.py
+++ Controlberry/temperature.py
@@ -17,7 +17,17 @@ DATABASE = json.loads(json_data)
 URI = DATABASE.get('URI')
 DB = DATABASE.get('Database')
 
-#setup mongodb
+#if URI doesnt exits it will write data to config.json
+if not URI:
+    URI = input("Please write your connection MongoDB URI and press Enter: \n")
+    DB = input("Please write name of your Database: \n")
+    with open(Config, 'w') as outfile:
+        json.dump({'URI':URI,'Database':DB}, outfile)
+
+json_data= open(Config).read()
+DATABASE = json.loads(json_data)
+URI = DATABASE.get('URI')
+DB = DATABASE.get('Database')
 CONNECTION = MongoClient(URI, connect = False)
 db = CONNECTION.get_database(DB)
 Temperature = db.Temperature
