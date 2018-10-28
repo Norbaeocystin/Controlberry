99cf74c4cfd748abf338eaf3f2033e54770a7b92
Norbaeocystin <baranrastislav@gmail.com>
2018-10-28 14:56:45 +0100

    Update camera.py

--- Controlberry/camera.py
+++ Controlberry/camera.py
@@ -1,32 +1,21 @@
 from io import BytesIO
 from time import sleep
 from picamera import PiCamera
-
-camera_running = {}
+from picamera.exc import PiCameraMMALError
 
 def get_image_as_bytes():
-'''
-returns image as bytes from Pi Camera
-'''
-  stream = BytesIO()
-  if not camera_running.get('camera'):
-    camera = PiCamera()
-    running['camera'] = camera
-    camera.start_preview()
-    sleep(2)
-    camera.capture(stream, format='jpeg')
-    stream.seek(0)
-    return stream.getvalue()  
-  if camera_running.get('camera'):
-    running.get('camera').capture(stream, format='jpeg')
-    stream.seek(0)
-    return stream.getvalue() 
-    
-
-def close_camera():
-  '''
-  if camera is running it will close the camera
-  '''
-  if camera_running.get('camera'):
-    camera_running.get('camera').close()
-    del camera_running['camera']
+    '''
+    returns image as bytes from Pi Camera
+    '''
+    stream = BytesIO()
+    try:
+        camera = PiCamera()
+        running['camera'] = camera
+        camera.start_preview()
+        sleep(1.3)
+        camera.capture(stream, format='jpeg')
+        stream.seek(0)
+        camera.close()
+        return stream.getvalue()
+    except PiCameraMMALError:
+        pass
