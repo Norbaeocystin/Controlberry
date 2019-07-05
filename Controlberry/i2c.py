93e0b4e2f710d59eb53aa7547b9238f6a20b9e04
Norbaeocystin <baranrastislav@gmail.com>
2019-07-05 22:05:06 +0200

    Update i2c.py

--- Controlberry/i2c.py
+++ Controlberry/i2c.py
@@ -5,37 +5,42 @@ bus = smbus.SMBus(1)
 OPERATIONS = {"+":"__add__","/":'__rtruediv__', "-":"__sub__", "x":"__rmul__", 
 	"^":"__rxor__","<<":"__rlshift__", ">>":"__rshift__" }
 
-def write_byte_to_address(bus = 1, byte = 0, address = 0x00):
-	smb = smbus.SMBus(bus)
-	smb.write_byte(address, byte)
 
 def read_bytes(bus = 1, address = 0x00, bytes_to_send = 0, length_bytes_received = 2):
 	"""
-
+	writes and return bytes
 	"""
 	smb = smbus.SMBus(bus)
 	bytes_returned = smb.read_i2c_block_data(address, bytes_to_send, length_bytes_received)
 	return bytes_returned
 
-example_operations = {0:[["x",128]], "total":[["/",1024"]]}
-def transform_bytes(bytes, operations = {0:[["+",2], ['/', 3]]}):
-	total = False
-	for i, item in enumerate(bytes):
-		value = item
-		oper = operations.get(i):
-		if oper:
-			for o in oper:
-				method = OPERATIONS[o[0]]
-				function = float.getattr(method)
-				value = function(value, o[1])
-		total += value
-	if operations.get('total'):
-		oper = operations.get(i):
-		if oper:
-			for o in oper:
-				method = OPERATIONS[o[0]]
-				function = float.getattr(method)
-				total = function(value, o[1])
-	return total
+example_operations = {0:[["/",128]], "total":[['/',1024]]}
+def transform_bytes(bytes_list, operations):
+	"""
+	transform received bytes by operations tuple, operations implemented: +-/* and bitewise >>, << and ^
+	"""
+    total = False
+    for i, item in enumerate(bytes_list):
+        value = float(item)
+        oper = operations.get(i)
+        if oper:
+            for o in oper:
+                method = OPERATIONS[o[0]]
+                function = getattr(float, method)
+                val = float(o[1])
+                value = function(val, value)
+                total += value
+        else:
+            total += value
+    if operations.get('total'):
+        total = float(total)
+        op = operations.get('total')
+        if op:
+            for o in op:
+                method = OPERATIONS[o[0]]
+                func = getattr(float, method)
+                val = float(o[1])
+                total = func(val, total)
+    return total
 		
 
