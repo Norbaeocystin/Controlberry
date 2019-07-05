124d59a5558fbb498c7a0c422689690f7fc38b1c
Norbaeocystin <baranrastislav@gmail.com>
2019-07-06 00:10:01 +0200

    Update i2c.py

--- Controlberry/i2c.py
+++ Controlberry/i2c.py
@@ -1,22 +1,22 @@
 import smbus
 
-OPERATIONS = {"+":"__add__","/":'__rtruediv__', "-":"__sub__", "x":"__rmul__", 
-	"^":"__rxor__","<<":"__rlshift__", ">>":"__rshift__" }
+OPERATIONS = {"+":"__add__","/":'__rtruediv__', "-":"__sub__", "x":"__rmul__", "^":"__rxor__","<<":"__rlshift__", ">>":"__rshift__" }
 
 
 def read_bytes(bus = 1, address = 0x00, bytes_to_send = 0, length_bytes_received = 2):
-	"""
-	writes and return bytes
-	"""
-	smb = smbus.SMBus(bus)
-	bytes_returned = smb.read_i2c_block_data(address, bytes_to_send, length_bytes_received)
-	return bytes_returned
+    """
+    writes and return bytes
+    """
+    smb = smbus.SMBus(bus)
+    bytes_returned = smb.read_i2c_block_data(address, bytes_to_send, length_bytes_received)
+    return bytes_returned
 
-example_operations = {0:[["/",128]], "total":[['/',1024]]}
+example_operations = {0:[["x",128]], "total":[['/',1024]]}
+
 def transform_bytes(bytes_list, operations):
-	"""
-	transform received bytes by operations tuple, operations implemented: +-/* and bitewise >>, << and ^
-	"""
+    """
+    transform received bytes by operations tuple, operations implemented: +-/* and bitewise >>, << and ^
+    """
     total = False
     for i, item in enumerate(bytes_list):
         value = float(item)
@@ -40,5 +40,3 @@ def transform_bytes(bytes_list, operations):
                 val = float(o[1])
                 total = func(val, total)
     return total
-		
-
